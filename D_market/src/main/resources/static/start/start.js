e1da7a8a33cfcac25301b166b76ff959175cf08a
MikhailFilippov1 <cp_filippov@mail.ru>
2022-12-06 09:54:13 +0300

    Cart is on own module now. Dto's also in own module. RestTemplate is using.

--- D_market/src/main/resources/static/start/start.js
+++
@@ -1,4 +0,0 @@
-angular.module('market-front').controller('startController', function ($scope, $http){
-    const contextPath = 'http://localhost:8189/market/';
-
-});
---
+++ D_market_MS/cart-service/src/main/java/ru/geekbrains/D_market/cart/DMarketCartApplication.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.D_market.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DMarketCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DMarketCartApplication.class, args);
+	}
+}
